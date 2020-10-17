1b87e8a4fd3b8016474a51c48b8da436ee7d957c
neogentrics <61997760+neogentrics@users.noreply.github.com>
2020-10-16 22:17:16 -0500

    Create FileCreation.java

---
+++ FileCreation.java
@@ -0,0 +1,24 @@
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+public class FileCreation {
+  public static void main(String[] args) {
+    try {
+      File myObj = new File("filename.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
+
+/* The process of creating a file in said directory.
+As this is just basic syntax. I am hoping to find a way to utalize it better.
+
+
+*/
