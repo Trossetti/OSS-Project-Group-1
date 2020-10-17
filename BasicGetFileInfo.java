ac2e6db02a53d83e21c4bd7b03fd4f3ffde3e54f
neogentrics <61997760+neogentrics@users.noreply.github.com>
2020-10-16 22:34:11 -0500

    Create BasicGetFileInfo.java

---
+++ BasicGetFileInfo.java
@@ -0,0 +1,25 @@
+import java.io.File;  // Import the File class
+
+public class BasicGetFileInfo { 
+  public static void main(String[] args) {
+    File myObj = new File("filename.txt");
+    if (myObj.exists()) {
+      System.out.println("File name: " + myObj.getName());
+      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+      System.out.println("Writeable: " + myObj.canWrite());
+      System.out.println("Readable " + myObj.canRead());
+      System.out.println("File size in bytes " + myObj.length());
+    } else {
+      System.out.println("The file does not exist.");
+    }
+  }
+}
+
+
+
+
+/* The process of creating a file in said directory.
+As this is just basic syntax. I am hoping to find a way to utalize it better.
+
+
+*/
