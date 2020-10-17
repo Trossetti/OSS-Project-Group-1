5e3d4f5ef289f90615417313a1d79383f3432a39
neogentrics <61997760+neogentrics@users.noreply.github.com>
2020-10-16 22:27:00 -0500

    Update and rename WriteToFile.java to BasicWriteFile.java

--- BasicWriteFile.java
+++ BasicWriteFile.java
@@ -1,7 +1,7 @@
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 
-public class WriteToFile {
+public class BasicWriteFile {
   public static void main(String[] args) {
     try {
       FileWriter myWriter = new FileWriter("filename.txt");
