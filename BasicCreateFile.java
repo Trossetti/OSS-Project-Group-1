9447a6f0948f0233d9006e515770cab3a6840da7
neogentrics <61997760+neogentrics@users.noreply.github.com>
2020-10-16 22:26:11 -0500

    Update and rename CreateFile.java to BasicCreateFile.java

--- BasicCreateFile.java
+++ BasicCreateFile.java
@@ -1,7 +1,7 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 
-public class CreateFile {
+public class BasicCreateFile {
   public static void main(String[] args) {
     try {
       File myObj = new File("filename.txt");
